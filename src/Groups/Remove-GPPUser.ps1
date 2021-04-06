a3ada0f152c8a25309755470196a85658fc0db66
Kirill Nikolaev <kf@exchange12rocks.org>
2021-04-06 23:58:56 +0300

    Groups: User: Adds a function to remove User definitions

---
+++ src/Groups/Remove-GPPUser.ps1
@@ -0,0 +1,47 @@
+function Remove-GPPUser {
+    [OutputType([System.Void])]
+    Param (
+        [Parameter(ParameterSetName = 'ByGPONameObjectName', Mandatory)]
+        [Parameter(ParameterSetName = 'ByGPOIdObjectName', Mandatory)]
+        [Parameter(ParameterSetName = 'ByGPPSectionObjectName', Mandatory)]
+        [string]$Name,
+        [Parameter(ParameterSetName = 'ByGPONameObjectLiteralName', Mandatory)]
+        [Parameter(ParameterSetName = 'ByGPOIdObjectLiteralName', Mandatory)]
+        [Parameter(ParameterSetName = 'ByGPPSectionObjectLiteralName', Mandatory)]
+        [string]$LiteralName,
+        [Parameter(ParameterSetName = 'ByGPONameBuiltInUser', Mandatory)]
+        [Parameter(ParameterSetName = 'ByGPOIdBuiltInUser', Mandatory)]
+        [Parameter(ParameterSetName = 'ByGPPSectionBuiltInUser', Mandatory)]
+        [GPPItemUserSubAuthority]$BuiltInUser,
+        [Parameter(ParameterSetName = 'ByGPONameObjectUID', Mandatory)]
+        [Parameter(ParameterSetName = 'ByGPOIdObjectUID', Mandatory)]
+        [Parameter(ParameterSetName = 'ByGPPSectionObjectUID', Mandatory)]
+        [guid]$UID,
+        [Parameter(ParameterSetName = 'ByGPPSectionObjectName', Mandatory)]
+        [Parameter(ParameterSetName = 'ByGPPSectionObjectLiteralName', Mandatory)]
+        [Parameter(ParameterSetName = 'ByGPPSectionBuiltInUser', Mandatory)]
+        [Parameter(ParameterSetName = 'ByGPPSectionObjectUID', Mandatory)]
+        [GPPSection]$GPPSection,
+        [Parameter(ParameterSetName = 'ByGPONameObjectName', Mandatory)]
+        [Parameter(ParameterSetName = 'ByGPONameObjectLiteralName', Mandatory)]
+        [Parameter(ParameterSetName = 'ByGPONameBuiltInUser', Mandatory)]
+        [Parameter(ParameterSetName = 'ByGPONameObjectUID', Mandatory)]
+        [string]$GPOName,
+        [Parameter(ParameterSetName = 'ByGPOIdObjectName', Mandatory)]
+        [Parameter(ParameterSetName = 'ByGPOIdObjectLiteralName', Mandatory)]
+        [Parameter(ParameterSetName = 'ByGPOIdBuiltInUser', Mandatory)]
+        [Parameter(ParameterSetName = 'ByGPOIdObjectUID', Mandatory)]
+        [guid]$GPOId,
+        [Parameter(ParameterSetName = 'ByGPONameObjectName')]
+        [Parameter(ParameterSetName = 'ByGPOIdObjectName')]
+        [Parameter(ParameterSetName = 'ByGPONameObjectLiteralName')]
+        [Parameter(ParameterSetName = 'ByGPOIdObjectLiteralName')]
+        [Parameter(ParameterSetName = 'ByGPONameBuiltInUser')]
+        [Parameter(ParameterSetName = 'ByGPOIdBuiltInUser')]
+        [Parameter(ParameterSetName = 'ByGPONameObjectUID')]
+        [Parameter(ParameterSetName = 'ByGPOIdObjectUID')]
+        [GPPContext]$Context = $ModuleWideDefaultGPPContext
+    )
+
+    Remove-GPPGroupsItem @PSBoundParameters -ItemType ([GPPItemUser])
+}
