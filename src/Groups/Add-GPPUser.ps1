bd42109918868cbf4790c293685ddb627abf091b
Kirill Nikolaev <kf@exchange12rocks.org>
2021-04-07 00:14:10 +0300

    Groups: User: A new function to add User definitions into GPO

---
+++ src/Groups/Add-GPPUser.ps1
@@ -0,0 +1,17 @@
+function Add-GPPUser {
+    [OutputType([System.Void])]
+    Param (
+        [Parameter(ParameterSetName = 'ByName', Mandatory)]
+        [Parameter(ParameterSetName = 'ById', Mandatory)]
+        [GPPItemGroup]$InputObject,
+        [Parameter(ParameterSetName = 'ByName', Mandatory)]
+        [string]$GPOName,
+        [Parameter(ParameterSetName = 'ById', Mandatory)]
+        [guid]$GPOId,
+        [Parameter(ParameterSetName = 'ByName')]
+        [Parameter(ParameterSetName = 'ById')]
+        [GPPContext]$Context = $ModuleWideDefaultGPPContext
+    )
+
+    Add-GPPGroupsItem @PSBoundParameters -Context $Context
+}
