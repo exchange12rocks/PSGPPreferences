36d6e75361f286b2a13719c0d7e781f1491d83f4
Kirill Nikolaev <git@exchange12rocks.org>
2021-04-19 11:06:07 +0300

    Get-GPPSectionFilePath: Adds a feature to return a folder path along with the file path. To accomodate new Set-GPPSection's functionality

--- src/Common/Get-GPPSectionFilePath.ps1
+++ src/Common/Get-GPPSectionFilePath.ps1
@@ -5,12 +5,24 @@ function Get-GPPSectionFilePath {
         [Parameter(Mandatory)]
         [GPPContext]$Context,
         [Parameter(Mandatory)]
-        [GPPType]$Type
+        [GPPType]$Type,
+        [switch]$Extended
     )
 
     $DomainName = (Get-CimInstance -ClassName 'Win32_ComputerSystem').Domain # Computer's domain name, not user's
     $PoliciesFolderPath = '\\{0}\SYSVOL\{0}\Policies' -f $DomainName
     $PolicyPath = Join-Path -Path $PoliciesFolderPath -ChildPath $GPOId.ToString('B') # B - 32 digits separated by hyphens, enclosed in braces: {00000000 - 0000 - 0000 - 0000 - 000000000000}
     $ContextPath = Join-Path -Path $PolicyPath -ChildPath ('{0}\Preferences' -f $Context)
-    Join-Path -Path $ContextPath -ChildPath ('{0}\{0}.xml' -f $Type)
+    $FolderPath = Join-Path -Path $ContextPath -ChildPath $Type
+    $FilePath = Join-Path -Path $FolderPath -ChildPath ('{0}.xml' -f $Type)
+
+    if ($Extended) {
+        @{
+            FolderPath = $FolderPath
+            FilePath = $FilePath
+        }
+    }
+    else {
+        $FilePath
+    }
 }
