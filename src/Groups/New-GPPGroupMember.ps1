e5bc74ec5ec364de9a393e8ce5a1bcd67a72a271
Kirill Nikolaev <GitHub@exchange12rocks.org>
2022-04-04 20:34:02 +0300

    New-GPPGroupMember: Allows to create Group Member items without SIDs

--- src/Groups/New-GPPGroupMember.ps1
+++ src/Groups/New-GPPGroupMember.ps1
@@ -11,7 +11,18 @@ function New-GPPGroupMember {
     )
 
     if ($Name) {
-        $SID = ([System.Security.Principal.NTAccount]::new($Name)).Translate([System.Security.Principal.SecurityIdentifier]).Value
+        try {
+            $SID = ([System.Security.Principal.NTAccount]::new($Name)).Translate([System.Security.Principal.SecurityIdentifier]).Value
+        }
+        catch {
+            # https://github.com/exchange12rocks/PSGPPreferences/issues/31
+            # Not all names should be resolved into SIDs. Especially names with GPP variables in them.
+
+            $SID = $null
+            if ($_.FullyQualifiedErrorId -ne 'IdentityNotMappedException') {
+                throw $_
+            }
+        }
     }
     else {
         $Name = [System.Security.Principal.SecurityIdentifier]::new($SID).Translate([System.Security.Principal.NTAccount]).Value
