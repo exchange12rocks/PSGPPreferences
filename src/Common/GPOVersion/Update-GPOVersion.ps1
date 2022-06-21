60a9dd5b8d0b9369c25736b22c52100512e7347c
Kirill Nikolaev <git@exchange12rocks.org>
2022-06-21 14:14:38 +0300

    Common: Decouples GPO retrieval from the rest of the code for reusing in the future

--- src/Common/GPOVersion/Update-GPOVersion.ps1
+++ src/Common/GPOVersion/Update-GPOVersion.ps1
@@ -4,12 +4,7 @@ function Update-GPOVersion {
         [guid]$Id
     )
 
-    $IdFormatted = $Id.ToString('B')
-    $RootDSE = ([System.DirectoryServices.DirectoryEntry]::new('LDAP://RootDSE'))
-    $DomainDN = $RootDSE.defaultNamingContext
-    $GPOLDAPPath = 'LDAP://CN={0},CN=Policies,CN=System,{1}' -f $IdFormatted, $DomainDN[0] # $DomainDN is a collection of a System.DirectoryServices.ResultPropertyValueCollection type with a single element inside it
-
-    $GPO = ([System.DirectoryServices.DirectoryEntry]::new($GPOLDAPPath))
+    $GPO = Get-GPOObject -Id $Id
     $GPOVersionNumber = $GPO.versionNumber[0] # System.DirectoryServices.PropertyValueCollection, that's why we have to access the first element
     $GPOVersionNumber++
     $GPO.versionNumber[0] = $GPOVersionNumber
