acea85a134bcbeaf12cc5a22d3680878fdb63e20
Kirill Nikolaev <git@exchange12rocks.org>
2022-06-21 18:27:08 +0300

    Common: CSE classes now live in a separate file for easier debugging

--- src/Definitions/Classes.ps1
+++ src/Definitions/Classes.ps1
@@ -299,64 +299,6 @@ class GPPItemFilterRunOnce : GPPItemFilter {
 class GPPItemFilterCollection : GPPItemFilter {
     [GPPItemFilter[]]$Members
 }
-
-class GpoCseSet {
-    [guid[]]$Tool
-    [guid]$CSE
-
-    GpoCseSet([guid] $CseId) {
-        $this.CSE = $CseId
-        $this.Tool = Get-GpoToolIdByCseId -Id $CseId
-    }
-
-    GpoCseSet([guid] $CseId, [guid[]] $ToolId) {
-        $this.CSE = $CseId
-        $this.Tool = $ToolId
-    }
-}
-
-class GPOExtensionNamesAttribute {
-    [GpoCseSet[]]$Members
-
-    GPOExtensionNamesAttribute([guid[]] $CseId) {
-        $this.Members = foreach ($Id in $CseId) {
-            [GpoCseSet]::new($Id)
-        }
-    }
-
-    GPOExtensionNamesAttribute([GpoCseSet[]] $Members) {
-        $this.Members = $Members
-    }
-
-    [string] ToString () {
-        $SortedTools = $this.Members.Tool | Sort-Object
-
-        $sb = [System.Text.StringBuilder]::new()
-        foreach ($Tool in $SortedTools) {
-            [void]$sb.Append($Tool.ToString('B'))
-        }
-        $SortedToolsString = $sb.ToString()
-
-        $SortedCSE = $this.Members.CSE | Sort-Object
-
-        $sb = [System.Text.StringBuilder]::new()
-        foreach ($CSE in $SortedCSE) {
-            $CurrentCSESet = $this.Members | Where-Object -FilterScript { $_.CSE -eq $CSE }
-            $CurrentCSEString = $CSE.ToString('B')
-            $SortedCSETools = $CurrentCSESet.Tool | Sort-Object
-            $sb2 = [System.Text.StringBuilder]::new()
-            foreach ($Tool in $SortedCSETools) {
-                [void]$sb2.Append($Tool.ToString('B'))
-            }
-            $SortedCSEToolsString = $sb2.ToString()
-            [void]$sb.Append(('[{0}{1}]' -f $CurrentCSEString, $SortedCSEToolsString))
-        }
-        $SortedCSEString = $sb.ToString()
-        #return $SortedCSEString
-        return '[{{00000000-0000-0000-0000-000000000000}}{0}]{1}' -f $SortedToolsString, $SortedCSEString
-    }
-}
-
 #endregion
 
 #region Groups
