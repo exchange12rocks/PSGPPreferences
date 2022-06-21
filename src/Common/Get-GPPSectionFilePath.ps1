9fd62c946bc78a4f24445347777cf8e2a84f8610
Kirill Nikolaev <git@exchange12rocks.org>
2022-06-21 16:47:03 +0300

    Common: Decouples GPO folder path extraction for future use

--- src/Common/Get-GPPSectionFilePath.ps1
+++ src/Common/Get-GPPSectionFilePath.ps1
@@ -9,9 +9,7 @@ function Get-GPPSectionFilePath {
         [switch]$Extended
     )
 
-    $DomainName = (Get-CimInstance -ClassName 'Win32_ComputerSystem').Domain # Computer's domain name, not user's
-    $PoliciesFolderPath = '\\{0}\SYSVOL\{0}\Policies' -f $DomainName
-    $PolicyPath = Join-Path -Path $PoliciesFolderPath -ChildPath $GPOId.ToString('B') # B - 32 digits separated by hyphens, enclosed in braces: {00000000 - 0000 - 0000 - 0000 - 000000000000}
+    $PolicyPath = Get-GPOFilePath -Id $Id
     $ContextPath = Join-Path -Path $PolicyPath -ChildPath ('{0}\Preferences' -f $Context)
     $FolderPath = Join-Path -Path $ContextPath -ChildPath $Type
     $FilePath = Join-Path -Path $FolderPath -ChildPath ('{0}.xml' -f $Type)
