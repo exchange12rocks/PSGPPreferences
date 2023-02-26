bee611623803cf81af28e3b191823bd2e0f18489
Kirill Nikolaev <git@exchange12.rocks>
2023-02-26 16:43:17 +0200

    First fix for #32

--- src/Common/CSE/Get-CseSetByType.ps1
+++
@@ -1,9 +0,0 @@
-function Get-CseSetByType {
-    Param (
-        [Parameter(Mandatory)]
-        [GPPType]$Type,
-        [PSCustomObject]$Definitions = $CSEDefinitions
-    )
-
-    $Definitions.$Type
-}
